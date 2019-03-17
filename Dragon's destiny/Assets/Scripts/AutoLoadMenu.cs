b20958ecec1da4dfc7e300ac151fded631a6214b
govnoigra <illusions47693@gmail.com>
2019-03-17 22:05:28 +0500

    first commit

---
+++ Dragon's destiny/Assets/Scripts/AutoLoadMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class AutoLoadMenu : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public void AutoLoadOurMenu()
+    {
+        
+        ulong numberFramesInVideo;
+        numberFramesInVideo = videoPlayer.frameCount;
+        ulong numberOfCurrentFrame;
+        numberOfCurrentFrame = (ulong) videoPlayer.frame;
+        if (numberOfCurrentFrame== numberFramesInVideo)
+        {
+            Application.LoadLevel(1);
+        }
+
+
+    }
+}
+
+
---
+++ Dragon's destiny/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel : MonoBehaviour
+{
+    public void Play()
+    {
+        Application.LoadLevel(2);
+    }
+
+    public void SkipIntro()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
