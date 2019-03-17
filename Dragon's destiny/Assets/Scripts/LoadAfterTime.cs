e60cb61e2bde31fca0ee9a9e7a27d5baec2f1156
govnoigra <illusions47693@gmail.com>
2019-03-18 00:02:57 +0500

    fifth commit

---
+++ Dragon's destiny/Assets/Scripts/LoadAfterTime.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAfterTime : MonoBehaviour
+{
+    private IEnumerator Wait(float seconds)
+    {
+        yield return new WaitForSeconds(5.0f); // таймер, через 10 секунд
+        Application.LoadLevel(1); // выполнится эта строка
+    }
+}
